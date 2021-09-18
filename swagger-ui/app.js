bfda40a977465bc5a729752efeef2664a12d19fc
Harish KM <harishkm@harishkm.in>
2021-09-18 21:49:47 +0530

    Move Swagger UI from /api-docs to just /

--- swagger-ui/app.js
+++ swagger-ui/app.js
@@ -2,14 +2,15 @@ const express = require('express')
 const serverless = require('serverless-http')
 const swaggerUI = require('swagger-ui-express')
 
-var options = {
-    swaggerOptions: {
-        url: process.env.SWAGGER_SPEC_URL
-    }
-}
-
 const app = express()
-app.use('/api-docs', swaggerUI.serve,
-    swaggerUI.setup(null, options))
+app.use(
+  '/',
+  swaggerUI.serve,
+  swaggerUI.setup(null, {
+    swaggerOptions: {
+      url: process.env.SWAGGER_SPEC_URL
+    }
+  })
+)
 
 module.exports.handler = serverless(app)
